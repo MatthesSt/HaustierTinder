a490d45424217d948355d0649e9a7fc8aecacbdf
MatthesSt <m.staacken@mbd-team.de>
2024-05-30 09:53:41 +0200

    wip jupiter notebook integration

---
+++ src/pages/JupiterNotebook.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Jupyter } from "jupyter-vue";
+const notebook = new Jupyter.Notebook();
+</script>
+<template>
+  <div>
+    <Jupyter :notebook="notebook" />
+  </div>
+</template>
+<style scoped lang="scss"></style>
--- src/router.ts
+++ src/router.ts
@@ -2,10 +2,12 @@ import { createRouter, createWebHashHistory } from "vue-router";
 
 import Home from "./pages/Home.vue";
 import Register from "./pages/Register.vue";
+import JupiterNotebook from "./pages/JupiterNotebook.vue";
 
 const routes = [
   { path: "/", component: Home },
   { path: "/register", component: Register },
+  { path: "/jupiter", component: JupiterNotebook },
 ];
 
 const router = createRouter({
