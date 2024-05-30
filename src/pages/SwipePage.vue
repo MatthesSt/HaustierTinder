57f8c7d9fbc80cc061aa5e13e3b328c89cd15309
Lemac86 <104212019+Lemac86@users.noreply.github.com>
2024-05-30 10:31:14 +0200

    Logo added, minor changes on components, new Route added, minor restr… (#31)
    * Logo added, minor changes on components, new Route added, minor restructuring
    
    * imports in App.vue removed
    
    * router import fixed

---
+++ src/pages/SwipePage.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <div class="w-100 d-flex justify-content-between">
+      <button type="button" class="btn btn-light color-black">
+        <i class="bi bi-list"></i>
+      </button>
+      <button type="button" class="btn btn-light">
+        <i class="bi bi-gear"></i>
+      </button>
+    </div>
+    <div id="carouselExample" class="carousel slide">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img
+            src="/chicken1.jpeg"
+            class="d-block w-100"
+            style="height: 800px"
+            alt="..."
+          />
+          <p class="m-3 fs-3">Claudia, 34</p>
+        </div>
+        <div class="carousel-item">
+          <img
+            src="/chicken2.jpg"
+            class="d-block w-100"
+            style="height: 800px"
+            alt="..."
+          />
+          <p class="m-3 fs-3">Herta, 22</p>
+        </div>
+        <div class="carousel-item">
+          <img
+            src="/chicken3.jpg"
+            class="d-block w-100"
+            style="height: 800px"
+            alt="..."
+          />
+          <p class="m-3 fs-3">Ingeborg, 19</p>
+        </div>
+      </div>
+      <button
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExample"
+        data-bs-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExample"
+        data-bs-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </div>
+</template>
--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 import Home from "./pages/Home.vue";
+import Swipe from "./pages/SwipePage.vue";
 import Register from "./pages/Register.vue";
 
 const routes = [
-  { path: "/", component: Home },
   { path: "/register", component: Register },
+  { path: "/", component: Home },
+  { path: "/swipe", component: Swipe },
 ];
 
 const router = createRouter({
