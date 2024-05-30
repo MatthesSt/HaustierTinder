7cc2f1a9ac78b2ea46d8d9fc15e97ba094e3d81b
Lemac86 <104212019+Lemac86@users.noreply.github.com>
2024-05-30 11:38:25 +0200

    Erste Anpassungen fürs Smartphone-Layout (#36)

--- src/pages/SwipePage.vue
+++ src/pages/SwipePage.vue
@@ -1,62 +1,65 @@
 <script setup lang="ts"></script>
 <template>
-  <div>
-    <div class="w-100 d-flex justify-content-between">
-      <button type="button" class="btn btn-light color-black">
-        <i class="bi bi-list"></i>
-      </button>
-      <button type="button" class="btn btn-light">
-        <i class="bi bi-gear"></i>
-      </button>
-    </div>
-    <div id="carouselExample" class="carousel slide">
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img
-            src="/chicken1.jpeg"
-            class="d-block w-100"
-            style="height: 800px"
-            alt="..."
-          />
-          <p class="m-3 fs-3">Claudia, 34</p>
-        </div>
-        <div class="carousel-item">
-          <img
-            src="/chicken2.jpg"
-            class="d-block w-100"
-            style="height: 800px"
-            alt="..."
-          />
-          <p class="m-3 fs-3">Herta, 22</p>
-        </div>
-        <div class="carousel-item">
-          <img
-            src="/chicken3.jpg"
-            class="d-block w-100"
-            style="height: 800px"
-            alt="..."
-          />
-          <p class="m-3 fs-3">Ingeborg, 19</p>
-        </div>
-      </div>
+  <div class="m-3">
+    <div class="w-100 d-flex justify-content-between mb-1">
       <button
-        class="carousel-control-prev"
-        type="button"
         data-bs-target="#carouselExample"
         data-bs-slide="prev"
-      >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button
-        class="carousel-control-next"
         type="button"
-        data-bs-target="#carouselExample"
-        data-bs-slide="next"
+        class="btn btn-light"
+        style="background-color: #ff3368; color: white; font-size: x-large"
       >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
+        <i class="bi bi-arrow-left"></i>
       </button>
+    </div>
+    <div class="rounded" style="background-color: #ff3368">
+      <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              src="/chicken1.jpeg"
+              class="d-block w-100 mt-3"
+              style="height: 80vh; object-fit: contain"
+              alt="..."
+            />
+            <p class="m-3 fs-3 text-white"><b>Claudia</b>, <i>34</i></p>
+          </div>
+          <div class="carousel-item">
+            <img
+              src="/chicken2.jpg"
+              class="d-block w-100 mt-3"
+              style="height: 80vh; object-fit: contain"
+              alt="..."
+            />
+            <p class="m-3 fs-3 text-white"><b>SingleMom34</b>, <i>29</i></p>
+          </div>
+          <div class="carousel-item">
+            <img
+              src="/chicken3.jpg"
+              class="d-block w-100 mt-3"
+              style="height: 80vh; object-fit: contain"
+              alt="..."
+            />
+            <p class="m-3 fs-3 text-white"><b>Crispy19</b>, <i>1</i></p>
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="prev"
+        >
+          <i class="bi bi-hand-thumbs-down fs-1"></i>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="next"
+        >
+          <i class="bi bi-heart fs-1"></i>
+        </button>
+      </div>
     </div>
   </div>
 </template>
