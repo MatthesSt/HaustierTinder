c2987be95a9637f378f608945edcc2018e9b7672
TimL135 <t.littschwager@mbd-team.de>
2024-06-06 18:27:44 +0000

    show error message

--- src/user.ts
+++ src/user.ts
@@ -13,11 +13,12 @@ export function addUser(user: User) {
 
 export function loginUser(email: string, password: string) {
   const user = getUsers().find(user => user.email == email && user.password == password);
-  if(user){
-        localStorage.setItem("current_user", JSON.stringify(user));
-        router.push("/");
-        console.log("The login was unsuccessful.")
-        return;
+  if (user) {
+    localStorage.setItem("current_user", JSON.stringify(user));
+    router.push("/");
+    console.log("The login was unsuccessful.")
+    return "";
   }
   console.log("no account with these credentials was found");
+  return "Es konnte kein Account mit diesen Anmeldedaten gefunden werden."
 }
