99dd7cda019682b5c8cc566f9fec8a98018e8174
TimL135 <88440453+TimL135@users.noreply.github.com>
2024-06-07 11:11:55 +0200

    Update src/pages/Register.vue
    Co-authored-by: dloetzke <97966457+dloetzke@users.noreply.github.com>

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -58,7 +58,7 @@ function saveUser() {
           <input type="text" class="form-control mb-3" placeholder="Benutzername" v-model="newUser.username" required />
           <input class="form-control mb-3" type="password" placeholder="Passwort" v-model="newUser.password" required />
           <input class="form-control mb-2" type="text" placeholder="E-Mail-Adresse" v-model="newUser.email" required />
-          <div class="">
+          <div>
             <div class="">Du hast bereits ein Konto?<span class="mx-1"></span><a href="/#/login">Zum Login</a></div>
             <div class="mt-3"><button class="btn btn-success">Registrieren</button></div>
           </div>
