47e34c104ca7f286dbaab4a6861b2f1f281c1bd0
dloetzke <97966457+dloetzke@users.noreply.github.com>
2024-06-07 11:15:11 +0200

    Implemented logout (#41)
    * Implemented logout
    
    * move user function in user.ts and save current user in users array in local storage
    
    * Update src/user.ts
    
    Co-authored-by: dloetzke <97966457+dloetzke@users.noreply.github.com>
    
    * Update src/user.ts
    
    Co-authored-by: dloetzke <97966457+dloetzke@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: TimL135 <t.littschwager@mbd-team.de>
    Co-authored-by: TimL135 <88440453+TimL135@users.noreply.github.com>

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
 import { routes } from "../router";
+import { logoutUser, currentUser } from "../user"
 
-const currentUser = localStorage.getItem("current_user");
 function checkActive(route: any) {
     return eval(route.active || '');
 }
 </script>
 
 <template>
-    <div class="d-flex" v-if="currentUser">
-        <div v-for="route of routes.filter(route => checkActive(route))" class="me-2 fs-1">
-            <router-link :to="route.path"><i :class="route.icon"></i></router-link>
+    <div class="d-flex justify-content-between" v-if="currentUser">
+        <div class="d-flex">
+            <div v-for="route of routes.filter(route => checkActive(route))" class="me-2 fs-1">
+                <router-link :to="route.path"><i :class="route.icon"></i></router-link>
+            </div>
         </div>
+        <button class="btn btn-danger" @click="logoutUser">
+            <i class="bi bi-door-closed"></i>
+        </button>
     </div>
 </template>
 
--- src/user.ts
+++ src/user.ts
@@ -1,5 +1,9 @@
 import { User } from "./types";
 import router from "./router.ts";
+import { ref, watch } from "vue";
+
+export const currentUser = ref<User | null>(null);
+loadCurrentUser();
 
 export function getUsers(): User[] {
   return JSON.parse(localStorage.getItem("users") || "[]");
@@ -13,11 +17,33 @@ export function addUser(user: User) {
 
 export function loginUser(email: string, password: string) {
   const user = getUsers().find(user => user.email == email && user.password == password);
-  if(user){
-        localStorage.setItem("current_user", JSON.stringify(user));
-        router.push("/");
-        console.log("The login was unsuccessful.")
-        return;
+  if (user) {
+    localStorage.setItem("current_user", JSON.stringify(user));
+    loadCurrentUser();
+    router.push("/");
+    console.log("The login was successful.")
+    return;
   }
   console.log("no account with these credentials was found");
+}
+function loadCurrentUser() {
+  currentUser.value = JSON.parse(localStorage.getItem("current_user") || "null");
+}
+watch(currentUser, setCurrentUser, { deep: true });
+
+function setCurrentUser() {
+  if (!currentUser.value) return;
+
+  localStorage.setItem('current_user', JSON.stringify(currentUser.value));
+  const users = getUsers();
+  const index = users.findIndex(user => user.id == currentUser.value?.id);
+  users[index] = currentUser.value;
+  localStorage.setItem('users', JSON.stringify(users));
+
+}
+export function logoutUser() {
+  localStorage.removeItem("current_user");
+  currentUser.value = null;
+  router.push("/login");
+  console.log("The logout was successful.")
 }
